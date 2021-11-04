2a85a1b39601329d794caabf8d4c2b249dc93d7f
Nguyen Ngoc Minh <nnminh432@gmail.com>
2021-11-05 04:21:32 +0700

    update

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ window.addEventListener('scroll',function(e) {
         scrollHeader.classList.remove('show');
     }
 })
+
+    // const clickBtn= document.querySelector('.btn');
+    // clickBtn.addEventListener('click',function(){
+    //     const content = document.querySelector('.content__second');
+        
+    // })
+    $(document).ready(function(){
+        $(".content__top--below a").click(function(event){
+            event.preventDefault();
+             post =$(this).attr('href');
+             position=$(post).offset().top;
+             $('html, body').animate({scrollTop: position-100},500);
+        })
+     })
